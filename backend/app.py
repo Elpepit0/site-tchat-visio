3e951425488965449d42bc052b2de2c8afb53da8
Yohan Boiroux <yohanboiroux@gmail.com>
2025-06-25 10:35:16 +0200

    + password hash

--- backend/app.py
+++ backend/app.py
@@ -55,7 +55,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
 with app.app_context():
     db.create_all()
